a884ec710ecff2e2ba7312ff23632b3df9d9d47a
Abhimanue Manchanda <abhim212000@yahoo.co.in>
2020-02-12 23:55:50 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,4 +5,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and i am learning"
+  puts "Hi, my name is #{name} and i am learning to program in #{language}."
+end
